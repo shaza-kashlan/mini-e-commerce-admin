ebc04560470039422f650fd480305cf2c48e3f37
shaza kashlan <eng.shazakashlan@gmail.com>
2024-03-07 09:25:19 +0100

    add missing code for sidebar

--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -4,19 +4,15 @@ import { AboutPage } from "../pages/AboutPage";
 
 const Sidebar = ({ isOpen }) => {
   return (
-    <div className="sidebar-list">
-      <Link to={<AboutPage/>}>Home</Link>
-
-      {/* {
-        <ul>
-          <li>
-            <a href="#"> Home </a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-        </ul>
-      } */}
+    <div className={`sidebar ${isOpen ? "open" : ""}`}>
+      <ul className="menu">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/About-US">About</Link>
+        </li>
+      </ul>
     </div>
   );
 };
