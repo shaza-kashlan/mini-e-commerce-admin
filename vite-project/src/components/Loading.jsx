f509350684613677f8c519147322c5dcea5b18f8
shaza kashlan <eng.shazakashlan@gmail.com>
2024-03-12 16:23:49 +0100

    add spinner Library

--- vite-project/src/components/Loading.jsx
+++ vite-project/src/components/Loading.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
 import React from "react";
+import { RainbowSpinner } from "react-spinners-kit";
 
 export const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <>
+      <div className="spinnerContainer">
+        <div className="spinner">
+          <RainbowSpinner size={100} color="purple" />
+        </div>
+        <h2>Loading...</h2>
+      </div>
+    </>
+  );
 };
